8efedecc7c6d08039c7346841a2a621c8e1c451f
Jonathan Parker <jon@roadhouse.com.au>
2020-08-30 12:31:00 +1000

    Cleanup

--- src/index.ts
+++ src/index.ts
@@ -3,102 +3,86 @@ import * as readline from 'readline';
 import * as fs from 'fs';
 import { argv } from 'process';
 
-class Runner {
-    public async run() {
+(async function run() {
+    const inputFile = argv[2];
+    const readInterface = readline.createInterface({ 
+        input: fs.createReadStream(inputFile)
+    });
 
-        const inputFile = argv[2];
-        const readInterface = readline.createInterface({ 
-            input: fs.createReadStream(inputFile)
-        });
+    const data: string[] = [];
+    
+    await readInterface.on('line', line => {
+        data.push(line);
+    });
 
-        const data: string[] = [];
-        
-        await readInterface.on('line', line => {
-            data.push(line);
-        });
+    await readInterface.on('close', () => {
+        const [initialLocationStr, commandsStr, ..._] = data;
+        const [initialDirectionStr, xStr, yStr] = initialLocationStr.split(' ');
 
-        await readInterface.on('close', () => {
-            const [initialLocationStr, commandsStr, ..._] = data;
-            const [initialDirectionStr, xStr, yStr] = initialLocationStr.split(' ');
+        let initialDirection: RobotControl.CompassReading;
+        switch (initialDirectionStr) {
+            case 'N':
+                initialDirection = 'N';
+                break;
+            case 'S':
+                initialDirection = 'S';
+                break;
+            case 'E':
+                initialDirection = 'E';
+                break;
+            case 'W':
+                initialDirection = 'W';
+                break
+            default:
+                throw Error(`Invalid initial direction ${initialDirectionStr}`);
+        }
+        const x = Number.parseInt(xStr);
+        const y = Number.parseInt(yStr);
 
-            let initialDirection: RobotControl.CompassReading;
-            switch (initialDirectionStr) {
-                case 'N':
-                    initialDirection = 'N';
-                    break;
-                case 'S':
-                    initialDirection = 'S';
-                    break;
-                case 'E':
-                    initialDirection = 'E';
-                    break;
-                case 'W':
-                    initialDirection = 'W';
-                    break
-                default:
-                    throw Error(`Invalid initial direction ${initialDirectionStr}`);
-            }
-            const x = Number.parseInt(xStr);
-            const y = Number.parseInt(yStr);
+        if (isNaN(x) || isNaN(y)) {
+            throw Error(`Invalid initial location ${xStr} ${yStr}`);
+        }
 
-            if (isNaN(x) || isNaN(y)) {
-                throw Error(`Invalid initial location ${xStr} ${yStr}`);
-            }
+        const initialLocation: RobotControl.Location = { x, y, orientation: initialDirection};
+        const isCommandChar = (char?:string): boolean => char == 'L' || char == 'R' || char == 'M';
+        const commandList: RobotControl.MoveCommand[] = [];
+        const commandStack: string[] = [];
+        const digitStack: number[] = [];
 
-            const initialLocation: RobotControl.Location = { x, y, orientation: initialDirection};
-            const isCommandChar = (char?:string): boolean => char == 'L' || char == 'R' || char == 'M';
-            const commandList: RobotControl.MoveCommand[] = [];
-            const commandStack: string[] = [];
-            const digitStack: number[] = [];
-
-            for(const char of commandsStr) {
-                //console.log(`stack lengths: command: ${commandStack.length}, digit: ${digitStack.length}`);
-                if (isCommandChar(char))
-                {
-                    if (commandStack.length || digitStack.length)
-                    {
-                        if (digitStack.length)
-                        {
-                            const lastDigit = digitStack.pop() || 0;
-                            const repeat = digitStack.length ? 
-                                (10 * (digitStack.pop() || 0)) + lastDigit :
-                                lastDigit;
-                            commandList.push({ type: RobotControl.parseCommandType(commandStack.pop() || ''), repeat });
-                        }
-                        else {
-                            commandList.push({ type: RobotControl.parseCommandType(commandStack.pop() || ''), repeat: 1 })
-                        }
-                    }
-                    commandStack.push(char);
-                }
-                else {
-                    digitStack.push(Number.parseInt(char));
-                }
-            }
-            if (digitStack.length)
+        for(const char of commandsStr) {
+            if (isCommandChar(char))
             {
-                const lastDigit = digitStack.pop() || 0;
-                const repeat = digitStack.length ? 
-                    (10 * (digitStack.pop() || 0)) + lastDigit :
-                    lastDigit;
-                commandList.push({ type: RobotControl.parseCommandType(commandStack.pop() || ''), repeat});
+                if (commandStack.length || digitStack.length)
+                {
+                    if (digitStack.length)
+                    {
+                        const lastDigit = digitStack.pop() || 0;
+                        const repeat = digitStack.length ? 
+                            (10 * (digitStack.pop() || 0)) + lastDigit :
+                            lastDigit;
+                        commandList.push({ type: RobotControl.parseCommandType(commandStack.pop() || ''), repeat });
+                    }
+                    else {
+                        commandList.push({ type: RobotControl.parseCommandType(commandStack.pop() || ''), repeat: 1 })
+                    }
+                }
+                commandStack.push(char);
             }
-            
-            console.log(JSON.stringify(initialLocation));
-            console.log('Commands:');
-            commandList.forEach(command => console.log(JSON.stringify(command)));
+            else {
+                digitStack.push(Number.parseInt(char));
+            }
+        }
+        if (digitStack.length)
+        {
+            const lastDigit = digitStack.pop() || 0;
+            const repeat = digitStack.length ? 
+                (10 * (digitStack.pop() || 0)) + lastDigit :
+                lastDigit;
+            commandList.push({ type: RobotControl.parseCommandType(commandStack.pop() || ''), repeat});
+        }
 
-            console.log('Initial location:')
-            console.log(JSON.stringify(initialLocation));
-            console.log('Command:')
-            console.log(JSON.stringify(commandList));
-            
-            const finalLocation = RobotControl.Robot(initialLocation, commandList);
-            
-            console.log('Final location:');
-            console.log(JSON.stringify(finalLocation));
-        });
-    }
-}
-
-new Runner().run()
+        const finalLocation = RobotControl.Robot(initialLocation, commandList);
+        
+        console.log(`Final location: ${finalLocation.orientation} ${finalLocation.x} ${finalLocation.y}`);
+    });
+})();
