e30b38c77cc21682e10041bb93a9eefce12b5b6b
Jonathan Parker <jon@roadhouse.com.au>
2020-09-08 09:53:17 +1000

    Refactoring

--- src/Robot.ts
+++ src/Robot.ts
@@ -1,28 +1,14 @@
 export namespace RobotControl {
 
-    enum CommandTypeEnum {
-        Rotate,
-        Move
-    };
+    enum CommandType { Rotate, Move }
+    enum DirectionValue { Left, Right }
+    const minX = 0, minY = 0, maxX = 99, maxY = 99;
+    type Increment = (num: number, increments: number) => number;
+    type Decrement = (num: number, increments: number) => number;
 
-    enum DirectionValue {
-        Left,
-        Right
-    }
-    
-    export interface RobotCommand {
-        CommandType: CommandTypeEnum
-        repeat: number
-    }
-
-    interface Rotate extends RobotCommand {
-        Direction: DirectionValue
-        CommandType: CommandTypeEnum.Rotate
-    }
-
-    interface Move extends RobotCommand {
-        CommandType: CommandTypeEnum.Move
-    }
+    export interface RobotCommand { CommandType: CommandType, repeat: number }
+    interface Rotate extends RobotCommand { Direction: DirectionValue, CommandType: CommandType.Rotate }
+    interface Move extends RobotCommand { CommandType: CommandType.Move }
 
     const RotateRobot = (currentLocation: Location, rotate: Rotate): Location => {
         const newLocation = { ...currentLocation, orientation: rotateMappings[currentLocation.orientation](rotate.Direction) };
@@ -44,10 +30,8 @@ export namespace RobotControl {
         }
     }
 
-    const isRotateCommand = (cmd: RobotCommand) : cmd is Rotate => cmd.CommandType == CommandTypeEnum.Rotate;
-    const isMoveCommand = (cmd: RobotCommand) : cmd is Move => cmd.CommandType == CommandTypeEnum.Move;
-    
-    // Take the current location of the robot and a list of commands and return the final location of the robot
+    const isRotateCommand = (cmd: RobotCommand) : cmd is Rotate => cmd.CommandType == CommandType.Rotate;
+    const isMoveCommand = (cmd: RobotCommand) : cmd is Move => cmd.CommandType == CommandType.Move;
     type RunRobot = (currentLocation: Location, command: RobotCommand[]) => Location;
 
     export const Robot: RunRobot = (currentLocation, commands) => 
@@ -60,52 +44,17 @@ export namespace RobotControl {
                 return acc;
         }, currentLocation);
 
-    // Grid boundaries
-    const minX = 0, minY = 0, maxX = 99, maxY = 99;
+    let incrementX: Increment = (x, incrementBy) => 
+        Array.from(Array(incrementBy).keys()).map(_ => 1).reduce((acc, _) => acc == maxX ? minX : acc + 1, x);
 
-    // Movement function definitions
-    type Increment = (num: number, increments: number) => number;
-    type Decrement = (num: number, increments: number) => number;
+    let decrementX: Decrement = (x, decrementBy) =>
+        Array.from(Array(decrementBy).keys()).reduce((acc, _) => acc == minX ? maxX : acc - 1, x);
+    
+    let incrementY: Increment = (y, incrementBy) => 
+        Array.from(Array(incrementBy).keys()).reduce((acc, _) => acc == maxY ? minY : acc + 1, y);
 
-    let incrementX: Increment = (x, incrementBy) => {
-        let finalVal = x;
-        while(incrementBy > 0)
-        {
-            finalVal = finalVal == maxX ? minX : finalVal + 1;
-            incrementBy--;
-        }
-        return finalVal;
-    }
-
-    let decrementX: Decrement = (x, decrementBy) => {
-        let finalVal = x;
-        while(decrementBy > 0)
-        {
-            finalVal = finalVal == minX ? maxX : finalVal - 1;
-            decrementBy--;
-        }
-        return finalVal;
-    }
-
-    let incrementY: Increment = (y, incrementBy) => {
-        let finalVal = y;
-        while(incrementBy > 0)
-        {
-            finalVal = finalVal == maxY ? minY : finalVal + 1;
-            incrementBy--;
-        }
-        return finalVal;
-    }
-
-    let decrementY: Decrement = (y, decrementBy) => {
-        let finalVal = y;
-        while(decrementBy > 0)
-        {
-            finalVal = finalVal == minY ? maxY : finalVal - 1;
-            decrementBy--;
-        }
-        return finalVal;
-    }
+    let decrementY: Decrement = (y, decrementBy) =>
+        Array.from(Array(decrementBy).keys()).reduce((acc, _) => acc == minY ? maxY : acc - 1, y);
 
     // Directions & Compass definitions
     export type CompassReading = 'N' | 'S' | 'E' | 'W';
@@ -131,11 +80,11 @@ export namespace RobotControl {
         const commandTypeStr = command.toUpperCase();
         switch (commandTypeStr) {
             case 'L':
-                return { Direction: DirectionValue.Left, repeat, CommandType: CommandTypeEnum.Rotate } as Rotate;
+                return { Direction: DirectionValue.Left, repeat, CommandType: CommandType.Rotate } as Rotate;
             case 'R':
-                return { Direction: DirectionValue.Right, repeat, CommandType: CommandTypeEnum.Rotate } as Rotate;
+                return { Direction: DirectionValue.Right, repeat, CommandType: CommandType.Rotate } as Rotate;
             case 'M':
-                return { repeat, CommandType: CommandTypeEnum.Move } as Move;
+                return { repeat, CommandType: CommandType.Move } as Move;
             default:
                 throw Error(`Invalid command ${commandTypeStr}`);
         }
