4b329aed08e5d48dc5b4d54f621fad1680264a15
Jonathan Parker <jon@roadhouse.com.au>
2020-09-16 20:21:40 +1000

    Added more tests and coverage

--- src/Robot.test.ts
+++ src/Robot.test.ts
@@ -6,9 +6,11 @@ describe('Robot', () => {
     test.each`
     initialLocation | commands              | expected
     ${'N 0 0'}      | ${'M1RM4L3M2'}        | ${'S 4 99'}
+    ${'S 0 0'}      | ${'M1RM4L3M2'}        | ${'N 96 1'}
     ${'N 99 99'}    | ${'M1M1M1M1M1'}       | ${'N 99 4'}
     ${'S 99 0'}     | ${'M1M1M1M1M1'}       | ${'S 99 95'}
     ${'W 0 0'}      | ${'M10M10M10M10M10'}  | ${'W 50 0'}
+    ${'E 0 0'}      | ${'M5RRR'}            | ${'N 5 0'}
     ${'W 0 0'}      | ${'M10L1M10M10M10'}   | ${'S 90 70'}
     `('Robot("$input.initialLocation", "$input.commands") - $input.expected', (row: DataRow) => {
         const result = Robot(row.initialLocation, row.commands)
@@ -16,4 +18,15 @@ describe('Robot', () => {
         if (!isError(result))
             expect(`${Compass[result.orientation]} ${result.x} ${result.y}`).toBe(row.expected)
     })
+})
+
+describe('Robot with invalid initial location', () => {
+    const initialLocation = 'M 0 0'
+    const commands = 'M1'
+    const expectedOutput = `Initial location ${initialLocation} could not be parsed`
+    const result = Robot(initialLocation, commands)
+    expect(isError(result)).toBeTruthy()
+    if (isError(result)) {
+        expect(result.message).toContain(expectedOutput)
+    }
 })
